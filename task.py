6882270312476c42491ba8896c736f30ca6b671b
aabbondandolo3 <a.abbondandolo3@studenti.unisa.it>
2026-05-25 16:23:58 +0200

    quarta sub-task non va con series(), restituisce il valore iniziale della funzione

--- task.py
+++ task.py
@@ -26,7 +26,8 @@ def calcola_polinomio_taylor(espressione: str, variabile: str, punto: float, ord
 
 def risolvi_sistema_lineare(eq1: str, eq2: str, var1: str, var2: str) -> Dict[sympy.Symbol, sympy.Expr]:
     """Sub-task 5: Risolvere un Sistema Lineare."""
-    pass
+    x,y = sympy.symbols('x, y,')
+    return sympy.linsolve([eq1, eq2],(x,y))
 
 def main():
     print("Sub-task 1:", calcola_derivata("x**3 + 2*x", "x"))
